04ec783897b91e77f91948e8938fe0fa1baca90a
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-24 17:09:53 +0530

    header mobile respon, added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,11 +6,11 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div
-      className="relative bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-white w-full flex px-52 
+      className="relative bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-white w-full flex px-4 md:px-52 
     py-3 items-center justify-between"
     >
       {/* left content */}
-      <div className="font-black text-2xl tracking-widest">AWS Quiz App - TS</div>
+      <div className="font-black text-lg md:text-2xl tracking-wide md:tracking-widest">AWS Quiz App - TS</div>
       <div className=" cursor-pointer">
         <DarkmodeToggler />
       </div>
