ce85ca22f9769386e0112c53ec0375d97c4a144e
J S Vignesh Kanna <vkrishitaran@gmail.com>
2022-12-24 19:57:28 +0530

    Header moved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import QuizContainer from "./components/QuizContainer";
 function App() {
   return (
     <div className="bg-slate-100 dark:bg-slate-900 min-h-screen text-slate-900 dark:text-white">
-      <Header />
       <QuizContainer />
     </div>
   );
